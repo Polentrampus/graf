31562b1a4467617faf89ac7ff5f4c8620b8c5a0d
Pshenicyna Polyna <pshenicynap@mail.ru>
2024-05-23 19:47:31 +0300

    двумерный массив вжух

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,49 +1,39 @@
 #include "GameEngine.h"
 #include "AssetManager.h"
-#define SIZEPOLE 10
 
 GameEngine::GameEngine() {
 	GameEngine::background.setFillColor(sf::Color::White);
 	
-	//cell.resize(SIZEPOLE);
-	//for (int i = 0; i < SIZEPOLE; i++) {
-	//	cell[i].resize(SIZEPOLE);
-	//	for (int j = 0; j < SIZEPOLE; j++) {
-	//		sf::Vector2f location(i * 50, j * 50);
-	//		cell[i][j] = Cell(location,AssetManager::GetTexture("image/fon_4.png"), AssetManager::GetTexture("image/vertix.png"),sf::Vector2f(50, 50));
-	//	}
-	//}
+	for (int i = 0; i < SIZEPOLE; i++) {
+		cell[i] = new Cell[SIZEPOLE];
+		for (int j = 0; j < SIZEPOLE; j++) {
+			sf::Vector2f location(i * 50+10, j * 50+10);
+			cell[i][j] = Cell(location, AssetManager::GetTexture("image/fon_4.png"), AssetManager::GetTexture("image/vertix.png"), sf::Vector2f(50, 50));
+  		}
+	}
 }
 
-//GameEngine::~GameEngine() {
-//	std::cout << "distructor game engine" << std::endl;
-//}
+GameEngine::~GameEngine() {
+	for (int i = 0; i < SIZEPOLE; ++i) {
+		delete[] cell[i];
+	}
+	delete[] cell;
+	std::cout << "distructor game engine" << std::endl;
+}
 
 void GameEngine::input()
 {
 	sf::Event event;
-	//sf::Vector2i mousPosition = sf::Mouse::getPosition(*window);
+	sf::Vector2i mousPosition = sf::Mouse::getPosition(*window);
 
 	while (window->pollEvent(event))
 	{
 		if (event.type == sf::Event::Closed) window->close();
 		if (event.type == sf::Event::MouseButtonPressed){
-			if (event.mouseButton.button == sf::Mouse::Right){
-				cell.checkClick(sf::Mouse::getPosition(*window));
-		//		if (cell.checkClick(sf::Mouse::getPosition(*window)))
-		//		{
-		//			CountVertix++;
-		//		}
-		//		/*int x = mousPosition.x / 50;
-		//		int y = mousPosition.y / 50;
-
-		//		cell.checkClick(sf::Vector2i(mousPosition.x, mousPosition.y));*/
-		//		//if (cell[x][y].getGlobalBounds().contains(mousPosition.x, mousPosition.y))//почему реагирует только 1-ая клетка?
-		//		//{
-		//		//	cell[x][y].checkClick(sf::Vector2i(mousPosition.x, mousPosition.y));
-		//		//	CountVertix++;
-		//		//	cell[x][y].NumVertex = CountVertix;
-		//		//}
+			if (event.mouseButton.button == sf::Mouse::Left){
+				int x = mousPosition.x / 50;
+				int y = mousPosition.y / 50;
+				cell[x][y].checkClick(sf::Mouse::getPosition(*window));
 			}
 		}
 	}
@@ -58,7 +48,7 @@ void GameEngine::draw()
 			window->draw(*cell[i][j].getSprite());
 		}
 	}*/
-	window->draw(*cell.getSprite());
+	window->draw(*cell[1][2].getSprite());
 	window->display();
 }
 void GameEngine::run()
--- GameEngine.h
+++ GameEngine.h
@@ -1,13 +1,16 @@
 ﻿#pragma once
 #include "Cell.h"
+#define SIZEPOLE 10
 
 class GameEngine {
 public:
 	GameEngine();
+	~GameEngine();
 	void run();
 	//std::vector<std::vector<Cell>> cell;
 	AssetManager manager;
-	Cell cell = Cell(sf::Vector2f(13,140), AssetManager::GetTexture("image/fon_4.png"), AssetManager::GetTexture("image/vertix.png"), sf::Vector2f(50, 50));
+	Cell** cell = new Cell* [SIZEPOLE];
+	//Cell cell = Cell(sf::Vector2f(13,140), AssetManager::GetTexture("image/fon_4.png"), AssetManager::GetTexture("image/vertix.png"), sf::Vector2f(50, 50));
 //private:
 	std::unique_ptr<sf::RenderWindow> window = std::make_unique<sf::RenderWindow>(sf::VideoMode(640, 640), L"Graph!", sf::Style::Close);
 	sf::RectangleShape background = sf::RectangleShape(sf::Vector2f(640, 640));
